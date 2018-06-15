1ae91e3cc63b39447723e92de3ad2805eb7a4656
Dmitriy Srkyl <35216878+Asangin@users.noreply.github.com>
2018-06-15 21:01:45 +0300

    impl part of MVVM pattern

--- app/src/main/java/com/studio/skryl/musicstreamingapp/MainActivity.java
+++ app/src/main/java/com/studio/skryl/musicstreamingapp/MainActivity.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.studio.skryl.musicstreamingapp.data.network.ApiMusicInterface;
 import com.studio.skryl.musicstreamingapp.model.Album;
 import com.studio.skryl.musicstreamingapp.model.Track;
-import com.studio.skryl.musicstreamingapp.model.Tracks;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,66 +17,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    Album album = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Config.API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        SCService scService = retrofit.create(SCService.class);
-        /*
-        scService.getRecentTrack(3135556).enqueue(new Callback<Track>() {
-            @Override
-            public void onResponse(Call<Track> call, Response<Track> response) {
-                if (response.isSuccessful()) {
-                    Track track = response.body();
-                    Toast.makeText(MainActivity.this, track.getmStreamURL().toString(), Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Error" + response.code(), Toast.LENGTH_SHORT).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<Track> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Network Error" + t.getMessage(), Toast.LENGTH_SHORT).show();
-                Log.e("tag", "Network Error" + t.getMessage());
-
-            }
-        });
-*/
-        scService.getRecentAlbums(1362526495, 10).enqueue(new Callback<Album>() {
-            @Override
-            public void onResponse(Call<Album> call, Response<Album> response) {
-                if (response.isSuccessful()){
-                    album = response.body();
-                    Log.d("tsg", album.toString());
-
-
-
-
-                } else {
-                    Toast.makeText(MainActivity.this, "Error" + response.code(), Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Album> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Network Error" + t.getMessage(), Toast.LENGTH_SHORT).show();
-                Log.e("tag", "Network Error" + t.getMessage());
-            }
-        });
 
 
     }
 
-    private void showMessage(String message) {
-        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-    }
+    
 }
